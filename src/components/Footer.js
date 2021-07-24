3fae52d1f1e49749ab1705818b1babd5f9bc8ed5
kashita145 <60257324+kashita145@users.noreply.github.com>
2021-07-24 21:34:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer">
       <span className="footer-text-main">RTB </span>Dashboard
       <br />
-      Made with <Icon icon={heartFilled} color="red" /> by{" "}
+      Made by{" "}
       <a href="https://github.com/archits581">Archit Shinde</a>,{" "}
       <a href="https://github.com/kashita145/">Kashita Talreja</a> and{" "}
       <a href="https://github.com/sauravUppoor">Saurav Uppoor</a>
