2c094ed7ff301d2881dc7ece5d51e996ee2acc4d
kashita145 <kashita.talreja@gmail.com>
2021-07-24 22:07:56 +0530

    remove icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Icon } from "@iconify/react";
+// import { Icon } from "@iconify/react";
 
 const Footer = () => {
   return (
     <div className="footer">
       <span className="footer-text-main">RTB </span>Dashboard
       <br />
-      Made by{" "}
-      <a href="https://github.com/archits581">Archit Shinde</a>,{" "}
+      Made by <a href="https://github.com/archits581">Archit Shinde</a>,{" "}
       <a href="https://github.com/kashita145/">Kashita Talreja</a> and{" "}
       <a href="https://github.com/sauravUppoor">Saurav Uppoor</a>
     </div>
